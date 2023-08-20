4ed1c93c40d0fc57476b928f1e319ca9ac88316c
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-08-20 08:49:04 +0630

    create the approutes getpage

---
+++ lib/utlis/app_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter_app/contants/routes.dart';
+import 'package:flutter_app/view/favourite.dart';
+import 'package:flutter_app/view/home.dart';
+import 'package:flutter_app/view/noti.dart';
+import 'package:flutter_app/view/user_profile.dart';
+import 'package:get/get.dart';
+
+appRoutes() => [
+      GetPage(
+        name: Routes.home,
+        page: () => HomePage(),
+        transition: Transition.native,
+        transitionDuration: const Duration(microseconds: 400),
+      ),
+      GetPage(
+        name: Routes.favourite,
+        page: () => Favourite(),
+        transition: Transition.native,
+        transitionDuration: const Duration(microseconds: 400),
+      ),
+      GetPage(
+        name: Routes.notification,
+        page: () => Notification(),
+        transition: Transition.native,
+        transitionDuration: const Duration(microseconds: 400),
+      ),
+      GetPage(
+        name: Routes.user,
+        page: () => UserProfile(),
+        transition: Transition.native,
+        transitionDuration: const Duration(microseconds: 400),
+      ),
+    ];
