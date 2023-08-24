bb67ff3af3672a82a50988cafd14b046e64220f3
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-08-24 11:30:08 +0630

    modify the model

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -7,22 +7,14 @@ class ProductModel {
   static const SIZE = "size";
   static const DETAILS = "details";
   static const INGREDIENTS = "ingredients";
-  static const INGREDIENT1 = "ingredient1";
-  static const INGREDIENT2 = "ingredient2";
-  static const INGREDIENT3 = "ingredient3";
-  static const INGREDIENT4 = "ingredient4";
 
   late String id;
   late String image;
   late String name;
   late String category;
-  late double price;
+  late String price; // Change this to String
   late int size;
   late String ingredients;
-  late String ingredient1;
-  late String ingredient2;
-  late String ingredient3;
-  late String ingredient4;
   late String details;
 
   ProductModel({
@@ -32,10 +24,6 @@ class ProductModel {
     required this.price,
     required this.category,
     required this.details,
-    required this.ingredient1,
-    required this.ingredient2,
-    required this.ingredient3,
-    required this.ingredient4,
     required this.ingredients,
     required this.size,
   });
@@ -48,10 +36,6 @@ class ProductModel {
     details = data[DETAILS];
     size = data[SIZE];
     ingredients = data[INGREDIENTS];
-    ingredient1 = data[INGREDIENT1];
-    ingredient2 = data[INGREDIENT2];
-    ingredient3 = data[INGREDIENT3];
-    ingredient4 = data[INGREDIENT4];
-    price = data[PRICE].toDouble();
+    price = data[PRICE] ?? "0"; // Initialize with "0" if it's null
   }
 }
