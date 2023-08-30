b4cb79c03dae66437454efa0543e97163df6b933
Soe Sandar Win <soesandarwin2201@gmail.com>
2023-08-30 11:18:22 +0630

    create the payment widget

--- lib/contants/instance.dart
+++ lib/contants/instance.dart
@@ -1,6 +1,10 @@
 import 'package:flutter_app/controller/auth_controller.dart';
+import 'package:flutter_app/controller/payment_controller.dart';
+import 'package:flutter_app/controller/product_controller.dart';
 
 import '../controller/card_controller.dart';
 
-CartController cartController = CartController.instance;
 AuthController authController = AuthController.instance;
+ProductController productController = ProductController.instance;
+CartController cartController = CartController.instance;
+PaymentsController paymentsController = PaymentsController.instance;
